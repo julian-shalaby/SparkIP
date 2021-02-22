48c2fbcd5defeec60450daef9a7a00313d8eabd2
Julian Shalaby <jshalaby@ucsc.edu>
2021-02-22 15:35:44 -0800

    cleaned a little

--- src/main/scala/com/databricks-115/Main.scala
+++ src/main/scala/com/databricks-115/Main.scala
@@ -4,18 +4,20 @@ import org.apache.spark.sql.functions.udf
 
 object Main extends App {
   val spark = SparkSession.builder()
-    .appName("DF Columns and Expressions")
+    .appName("IPAddress DataType")
     .config("spark.master", "local")
     .getOrCreate()
 
+  //load the json and create a view to query
   val path = "src/main/scala/com/databricks-115/IPText.json"
   val IPv4DF = spark.read.json(path)
-
   IPv4DF.createOrReplaceTempView("IPv4")
 
-  // UDF in a WHERE clause
-  val IPNetUDF = udf((x: String, y: String) => IPNetwork(x).netContainsIP(IPv4(y)))
+  // function and function registration to check if the IP address is in the IP network
+  val IPNetUDF = udf((IPNet: String, IPAddr: String) => IPNetwork(IPNet).netContainsIP(IPv4(IPAddr)))
   spark.udf.register("IPNetUDF", IPNetUDF)
+
+  //query to test the function
   spark.sql("SELECT * FROM IPv4 WHERE IPNetUDF('192.0.0.0/24', IPAddress)").show()
 
 }
