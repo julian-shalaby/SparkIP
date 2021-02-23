70d84159bda1310008978c7834091d073b3b1c77
Julian Shalaby <jshalaby@ucsc.edu>
2021-02-23 14:12:27 -0800

    updated functions

--- src/main/scala/com/databricks-115/Main.scala
+++ src/main/scala/com/databricks-115/Main.scala
@@ -10,7 +10,7 @@ import org.apache.spark.sql.functions.udf
     WHERE IPAddress IN '192.0.0.0/16'
     //takes the IPAddress argument into a function that runs IPNetwork's netContains function
 
-    val IPSet1 = IPSet('192.0.0.0/16', '0.0.0.0', '3.44.5.11.2')
+    val IPSet1 = IPSet(Seq("212.222.131.201", "212.222.131.200", "192.0.0.0/16"))
     SELECT *
     FROM log
     WHERE IPAddress IN IPSet1
@@ -36,11 +36,36 @@ object Main extends App {
   val IPv4DF = spark.read.json(path)
   IPv4DF.createOrReplaceTempView("IPv4")
 
-  // function and function registration to check if the IP address is in the IP network
-  val IPNetUDF = udf((IPAddr: String, IPNet: String) => IPNetwork(IPNet).netContainsIP(IPv4(IPAddr)))
-  spark.udf.register("IPNetUDF", IPNetUDF)
-
+  //function and function registration to check if the IP address is in the IP network
+  val IPNetContains = udf((IPAddr: String, IPNet: String) => IPNetwork(IPNet).netContainsIP(IPv4(IPAddr)))
+  spark.udf.register("IPNetContains", IPNetContains)
   //query to test the function
-  spark.sql("SELECT * FROM IPv4 WHERE IPNetUDF(IPAddress, '192.0.0.0/24')").show()
+  spark.sql(
+    "SELECT * " +
+    "FROM IPv4 " +
+    "WHERE IPNetContains(IPAddress, '192.0.0.0/24')"
+  ).show()
+
+//  //passing objects to UDFs isn't working for some reason?
+//  val IPSet1 = IPSet(Seq("212.222.131.201", "212.222.131.200", "192.0.0.0/16"))
+//  //function and function registration to check if the IP address is in the IP Set
+//  val IPSetContains = udf((IPAddr: String, IPSetObj: IPSet) => IPSetObj.contains(IPv4(IPAddr)))
+//  spark.udf.register("IPSetContains", IPSetContains)
+//  //query to test the function
+//  spark.sql(
+//    "SELECT * " +
+//      "FROM IPv4 " +
+//      s"WHERE IPSetContains(IPAddress, $IPSet1)"
+//  ).show()
+
+  //function and function registration to check if the IP address is a multicast one
+  val IPIsMulticast = udf((IPAddr: String) => IPv4(IPAddr).isMulticast)
+  spark.udf.register("IPIsMulticast", IPIsMulticast)
+  //query to test the function
+  spark.sql(
+    "SELECT * " +
+      "FROM IPv4 " +
+      "WHERE IPIsMulticast(IPAddress)"
+  ).show()
 
 }
