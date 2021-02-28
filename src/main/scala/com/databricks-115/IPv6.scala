741206488fda860d1e6d1bd68f3c88345d4bfbd8
Julian Shalaby <jshalaby@ucsc.edu>
2021-02-27 18:21:39 -0800

    IPv6 Address Types

--- src/main/scala/com/databricks-115/IPv6.scala
+++ src/main/scala/com/databricks-115/IPv6.scala
@@ -28,19 +28,43 @@ case class IPv6 (addr: String) extends IPAddress {
   def <=(that: IPv6): Boolean = this.addrBI <= that.addrBI
   def >=(that: IPv6): Boolean = this.addrBI >= that.addrBI
 
+  //Address Types
+  val isLinkLocal: Boolean = if (addrBI >= new BigInteger("338288524927261089654018896841347694592") &&
+    addrBI <= new BigInteger("338620831926207318622244848606417780735")) true else false
+  val isLoopback: Boolean = if (addrBI == new BigInteger("1")) true else false
+  val isMulticast: Boolean = if (addrBI >= new BigInteger("338953138925153547590470800371487866880") &&
+    addrBI <= new BigInteger("340282366920938463463374607431768211455")) true else false
+  val isUnspecified: Boolean = if (addrBI == new BigInteger("0")) true else false
+  val isUniqueLocal: Boolean = if (addrBI >= new BigInteger("334965454937798799971759379190646833152") &&
+    addrBI <= new BigInteger("337623910929368631717566993311207522303")) true else false
+  val isReserved: Boolean = if (
+    isUnspecified ||
+      isLoopback ||
+      (addrBI >= new BigInteger("281470681743360") && addrBI <= new BigInteger("281474976710655")) ||
+      (addrBI >= new BigInteger("18446462598732840960") && addrBI <= new BigInteger("18446462603027808255")) ||
+      (addrBI >= new BigInteger("524413980667603649783483181312245760") && addrBI <= new BigInteger("524413980667603649783483185607213055")) ||
+      (addrBI >= new BigInteger("1329227995784915872903807060280344576") && addrBI <= new BigInteger("1329227995784915891350551133989896191")) ||
+      (addrBI >= new BigInteger("42540488161975842760550356425300246528") && addrBI <= new BigInteger("42540488241204005274814694018844196863")) ||
+      (addrBI >= new BigInteger("42540490697277043217009159418706657280") && addrBI <= new BigInteger("42540491964927643445238560915409862655")) ||
+      (addrBI >= new BigInteger("42540766411282592856903984951653826560") && addrBI <= new BigInteger("42540766490510755371168322545197776895")) ||
+      (addrBI >= new BigInteger("42545680458834377588178886921629466624") && addrBI <= new BigInteger("42550872755692912415807417417958686719")) ||
+      isUniqueLocal ||
+      isLinkLocal ||
+      isMulticast
+  ) true else false
 
   /*
     Stub Functions
     Impl Later
    */
-  val isGlobal: Boolean = false
   def isIP(ip: String): Boolean = false
-  val isLinkLocal: Boolean = false
-  val isLoopback: Boolean = false
-  val isMulticast: Boolean = false
-  val isPrivate: Boolean = false
-  val isReserved: Boolean = false
-  val isUnspecified: Boolean = false
+
+
+
+
   def mask(maskIP: String): com.databricks115.IPAddress = IPv6("0")
   def toNetwork: IPNetwork = IPNetwork("0.0.0.0")
+
+  override val isPrivate: Boolean = false
+  override val isGlobal: Boolean = false
 }
--- src/test/scala/com/databricks-115/TestIPv6.scala
+++ src/test/scala/com/databricks-115/TestIPv6.scala
@@ -1,5 +1,6 @@
 package com.databricks115
 import org.scalatest.FunSuite
+import java.math.BigInteger
 
 class TestIPv6Address extends FunSuite with SparkSessionTestWrapper{
   test("== - success") {
@@ -64,12 +65,97 @@ class TestIPv6Address extends FunSuite with SparkSessionTestWrapper{
 
   test("IP to BigInt - success") {
     val ip = IPv6("3001:db8:3333:4444:5555:6666:7777:8888")
-    assert(ip.addrBI.toString == "63808414359686959922502208113658529928")
+    assert(ip.addrBI == new BigInteger("63808414359686959922502208113658529928"))
   }
 
   test("IP to BigInt - failure") {
     val ip = IPv6("3001:db8:3333:4444:5555:6666:7777:8888")
-    assert(ip.addrBI.toString != "63808414359686959922502208113658529920")
+    assert(ip.addrBI != new BigInteger("63808414359686959922502208113658529920"))
+  }
+
+  /*
+Link Local:
+  fe80:: to febf:ffff:ffff:ffff:ffff:ffff:ffff:ffff
+  338288524927261089654018896841347694592 to 338620831926207318622244848606417780735
+ */
+  test("Link Local Addresses") {
+    val linkLocalIPs = List(
+      IPv6("fe80:0:0:0:0:0:0:0"),
+      IPv6("febf:ffff:ffff:ffff:ffff:ffff:ffff:ffff")
+    )
+    assert(linkLocalIPs.forall(ip => ip.isLinkLocal))
+  }
+  test("Not Link Local Addresses") {
+    val linkLocalIPs = List(
+      IPv6("fe70:0:0:0:0:0:0:0"),
+      IPv6("feff:ffff:ffff:ffff:ffff:ffff:ffff:ffff")
+    )
+    assert(linkLocalIPs.forall(ip => !ip.isLinkLocal))
+  }
+
+  /*
+Unspecified:
+  ::
+  0
+ */
+  test("Unspecified Addresses") {
+    val unspecifiedIP = IPv6("0:0:0:0:0:0:0:0")
+    assert(unspecifiedIP.isUnspecified)
+  }
+  test("Not Unspecified Addresses") {
+    val unspecifiedIP = IPv6("1:0:0:0:0:0:0:0")
+    assert(!unspecifiedIP.isUnspecified)
+  }
+
+  /*
+Loopback:
+  0:0:0:0:0:0:0:1
+  1
+ */
+  test("Loopback Addresses") {
+    val loopBackIP = IPv6("0:0:0:0:0:0:0:1")
+    assert(loopBackIP.isLoopback)
+  }
+  test("Not Loopback Addresses") {
+    val loopBackIP = IPv6("1:0:0:0:0:0:0:1")
+    assert(!loopBackIP.isLoopback)
+  }
+
+  /*
+Unique:
+  fc00:: to fdff:ffff:ffff:ffff:ffff:ffff:ffff:ffff
+  334965454937798799971759379190646833152 to 337623910929368631717566993311207522303
+ */
+  test("Unique Addresses") {
+    val uniqueLocalIPs = List(
+      IPv6("fc00:0:0:0:0:0:0:0"),
+      IPv6("fdff:ffff:ffff:ffff:ffff:ffff:ffff:ffff")
+    )
+    assert(uniqueLocalIPs.forall(ip => ip.isUniqueLocal))
+  }
+  test("Not Unique Addresses") {
+    val uniqueLocalIPs = List(
+      IPv6("fb00:0:0:0:0:0:0:0"),
+      IPv6("feff:ffff:ffff:ffff:ffff:ffff:ffff:ffff")
+    )
+    assert(uniqueLocalIPs.forall(ip => !ip.isUniqueLocal))
+  }
+
+  /*
+Multicast:
+  ff00:: to ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff
+  338953138925153547590470800371487866880 to 340282366920938463463374607431768211455
+ */
+  test("Multicast Addresses") {
+    val multicastIPs = List(
+      IPv6("ff00:0:0:0:0:0:0:0"),
+      IPv6("ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff")
+    )
+    assert(multicastIPs.forall(ip => ip.isMulticast))
+  }
+  test("Not Multicast Address") {
+    val multicastIP = IPv6("fe00:0:0:0:0:0:0:0")
+    assert(!multicastIP.isMulticast)
   }
 
 }
