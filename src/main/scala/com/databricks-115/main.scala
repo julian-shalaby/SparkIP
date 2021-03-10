bc1861763ef3c045c0ca1dba8b75f229dff190ae
Julian Shalaby <jshalaby@ucsc.edu>
2021-03-10 07:40:39 -0800

    .

--- src/main/scala/com/databricks-115/main.scala
+++ src/main/scala/com/databricks-115/main.scala
@@ -9,9 +9,21 @@ object main extends App {
     .getOrCreate()
   import spark.implicits._
 
+  def timeFunc(funcToTime: () => Unit) {
+    val t0 = System.nanoTime()
+    funcToTime()
+    val t1 = System.nanoTime()
+    val elapsed = t1 - t0
+    println(s"Elapsed time: $elapsed ns or ${elapsed/1000000000} sec")
+  }
+
   val path = "src/test/scala/com/databricks-115/IPText.json"
   val IPv4DS: Dataset[IPv4] = spark.read.json(path).as[IPv4]
 
-  IPv4DS.filter(ip => ip.isMulticast).show()
+  val network1 = IPv4Network("192.0.0.0/17")
+  //why does this take so long?
+  timeFunc(() => IPv4DS.filter(ip => network1.netContainsIP(ip)).show())
+  //but this is fast
+  timeFunc(() => IPv4DS.filter(ip => ip.isMulticast).show())
 
 }
