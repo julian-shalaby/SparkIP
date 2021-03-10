adce693c4aee568a94d98ef9c74b4d097904d3b6
Julian Shalaby <jshalaby@ucsc.edu>
2021-03-10 13:07:28 -0800

    using spark.time

--- src/test/scala/com/databricks-115/TestSparkUDF.scala
+++ src/test/scala/com/databricks-115/TestSparkUDF.scala
@@ -23,14 +23,6 @@ class TestSparkUDF extends FunSuite {
   IPv4DF.createOrReplaceTempView("IPv4")
   IPv4DFSmall.createOrReplaceTempView("IPv4Small")
 
-  def timeFunc(funcToTime: () => Unit) {
-    val t0 = System.nanoTime()
-    funcToTime()
-    val t1 = System.nanoTime()
-    val elapsed = t1 - t0
-    println(s"Elapsed time: $elapsed ns or ${elapsed/1000000000} sec")
-  }
-
   //UDFs
   //function and function registration to check if the IP address is in the IP network
   val IPNetContains: UserDefinedFunction = udf((IPNet: String, IPAddr: String) => IPv4Network(IPNet).netContainsIP(IPv4(IPAddr)))
@@ -41,66 +33,26 @@ class TestSparkUDF extends FunSuite {
 
   test("IPNetwork contains /17") {
     //using func
-    timeFunc(() =>
-      spark.sql(
-      s"""SELECT *
+      spark.time(
+        spark.sql(
+        s"""SELECT *
          FROM IPv4Small
          WHERE IPNetContains("192.0.0.0/17", IPAddress)"""
-    ).show())
+        ).show()
+      )
     //using regex
-    timeFunc(() =>
+    spark.time(
       spark.sql(
       """SELECT *
          FROM IPv4Small
          WHERE IPAddress RLIKE '^192\.0\.([0-9]|[0-9][0-9]|1[0-1][0-9]|12[0-7])\.[0-9]+$'"""
-    ).show())
-  }
-  test("IPNetwork contains /17 1 conversion") {
-    //function and function registration to check if the IP address is in the IP network
-    val IPNet = IPv4Network("192.0.0.0/17")
-    val IPNetContains1Conversion = udf((IPAddr: String) => IPNet.netContainsIP(IPv4(IPAddr)))
-    spark.udf.register("IPNetContains1Conversion", IPNetContains1Conversion)
-
-    //using func
-    timeFunc(() =>
-      spark.sql(
-        s"""SELECT *
-         FROM IPv4
-         WHERE IPNetContains1Conversion(IPAddress)"""
-      ).show())
-    //using regex
-    timeFunc(() =>
-      spark.sql(
-        """SELECT *
-         FROM IPv4
-         WHERE IPAddress RLIKE '^192\.0\.([0-9]|[0-9][0-9]|1[0-1][0-9]|12[0-7])\.[0-9]+$'"""
-      ).show())
-  }
-  test("IPNetwork contains /17 no conversion") {
-    val IPNet = IPv4Network("192.0.0.0/17")
-    val IPAddr = IPv4("192.0.78.70")
-    val IPNetContainsNoConversion = udf(() => IPNet.netContainsIP(IPAddr))
-    spark.udf.register("IPNetContainsNoConversion", IPNetContainsNoConversion)
-
-    //using func
-    timeFunc(() =>
-      spark.sql(
-        s"""SELECT *
-         FROM IPv4
-         WHERE IPNetContainsNoConversion()"""
-      ).show())
-    //using regex
-    timeFunc(() =>
-      spark.sql(
-        """SELECT *
-         FROM IPv4
-         WHERE IPAddress RLIKE '^192\.0\.([0-9]|[0-9][0-9]|1[0-1][0-9]|12[0-7])\.[0-9]+$'"""
-      ).show())
+      ).show()
+    )
   }
 
   test("IP is Multicast") {
     //function
-    timeFunc(() =>
+    spark.time(
       spark.sql(
         """SELECT *
         FROM IPv4
@@ -108,13 +60,14 @@ class TestSparkUDF extends FunSuite {
       ).show()
     )
     //regex
-    timeFunc(() =>
+    spark.time(
       spark.sql(
         """SELECT *
         FROM IPv4
         WHERE IPAddress RLIKE '(23[0-9]|22[4-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}'"""
       ).show()
     )
+
   }
 
 }
