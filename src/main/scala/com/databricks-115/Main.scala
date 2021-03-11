f9dd5554dba81b3fe9d1ac74f0cd91f0fd1bc5ff
Julian Shalaby <jshalaby@ucsc.edu>
2021-03-11 12:18:25 -0800

    update

--- src/main/scala/com/databricks-115/Main.scala
+++ src/main/scala/com/databricks-115/Main.scala
@@ -17,12 +17,12 @@ object Main extends App {
   val network3 = IPv4Network("0.0.0.0/17")
 
   //why does this take so long?
-  IPv4DS.filter(ip => network1.netContainsIP(ip)).show()
+  spark.time(IPv4DS.filter(ip => network1.netContainsIP(ip)).show())
   //and this
-  IPv4DS.filter(ip => network3.netContainsIP(ip)).show()
+  spark.time(IPv4DS.filter(ip => network3.netContainsIP(ip)).show())
   //but this is fast
-  IPv4DS.filter(ip => ip.isMulticast).show()
+  spark.time(IPv4DS.filter(ip => ip.isMulticast).show())
   //and this is fast
-  IPv4DS.filter(ip => network2.netContainsIP(ip)).show()
+  spark.time(IPv4DS.filter(ip => network2.netContainsIP(ip)).show())
 
 }
