e495c8368f40e6163949b1c9d485a8e5066560e5
Julian Shalaby <jshalaby@ucsc.edu>
2021-03-13 14:36:26 -0800

    update

--- spark/sql/catalyst/src/main/scala/org/apache/spark/sql/types/IPv4Type.scala
+++ spark/sql/catalyst/src/main/scala/org/apache/spark/sql/types/IPv4Type.scala
@@ -19,135 +19,41 @@ package org.apache.spark.sql.types
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.expressions.GenericInternalRow
 
-@SQLUserDefinedType(udt = classOf[IPUDT])
-private[sql] trait IPv4 extends Serializable {
-  def addr: String
-  def addrL: Long
+@SQLUserDefinedType(udt = classOf[IPv4TypeUDT])
+private[sql] class IPv4Type(val IPAddress: String) extends Serializable {
+  override def hashCode(): Int = classOf[IPv4TypeUDT].getName.hashCode()
+  override def equals(other: Any): Boolean = other match {
+    case that: IPv4Type => this.IPAddress == that.IPAddress
+    case _ => false
+  }
+  def IPv4ToLong(ip: String): Long = ip.split("\\.").reverse.zipWithIndex.map(a => a._1.toInt * math.pow(256, a._2).toLong).sum
+  val addrL = IPv4ToLong(IPAddress)
 }
 
-private[sql] class IPUDT extends UserDefinedType[IPv4] {
+/**
+ * User-defined type for [[IPv4]].
+ */
+private[sql] class IPv4TypeUDT extends UserDefinedType[IPv4Type] {
 
-  //for SQL datatype stuff. idk what to do yet
   override def sqlType: StructType = {
     StructType(Seq(
-      StructField("addr", StringType, nullable = false)
+      StructField("IPAddress", StringType, nullable = false)
     ))
   }
-  override def serialize(obj: IPv4): InternalRow = {
+  override def serialize(obj: IPv4Type): InternalRow = {
     val row = new GenericInternalRow(1)
-    obj match {
-      case _ =>
-        row.setString(0, "0.0.0.0")
-    }
+    row.setString(0, obj.IPAddress)
     row
   }
-  override def deserialize(datum: Any): IPv4 = {
+  override def deserialize(datum: Any): IPv4Type = {
     datum match {
-      case _ => new IPv4Type("0.0.0.0")
+      case row: InternalRow =>
+        new IPv4Type(row.getString(0))
     }
   }
-
-  //needed for udt i think
-  override def userClass: Class[IPv4] = classOf[IPv4]
-  private[spark] override def asNullable: IPUDT = this
-
-  override def equals(o: Any): Boolean = {
-    o match {
-      case _ => true
-    }
-  }
-
-  override def hashCode(): Int = classOf[IPUDT].getName.hashCode()
-
   override def typeName: String = "ip"
 
+  override def userClass: Class[IPv4Type] = classOf[IPv4Type]
+
+  private[spark] override def asNullable: IPv4TypeUDT = this
 }
-
-@SQLUserDefinedType(udt = classOf[IPUDT])
-class IPv4Type (val addr: String) extends IPv4 {
-  def IPv4ToLong(ip: String): Long = ip.split("\\.").reverse.zipWithIndex.map(a => a._1.toInt * math.pow(256, a._2).toLong).sum
-  override def addrL: Long = IPv4ToLong(addr)
-}
-
-object IPv4Type {
-  def apply(addr: String): IPv4Type = {
-    new IPv4Type(addr)
-  }
-}
-
-/*
-./bin/spark-shell --jars /Users/julianshalaby/IdeaProjects/Databricks-115/spark/sql/catalyst/target/spark-catalyst_2.12-3.2.0-SNAPSHOT.jar
-
-import org.apache.spark.sql.types._
-
-val ip: IPv4Type = new IPv4Type("192.0.0.0")
-
-val ip2 = Seq(
-  new IPv4Type("192.0.0.0"),
-  new IPv4Type("0.0.0.0"),
-  new IPv4Type("78.0.6.0")
-).toDF
-
-ip2.select($"addr").show()
- */
-
-/*
-import org.apache.spark.sql.{DataFrame, SparkSession}
-
-  val path = "/Users/julianshalaby/IdeaProjects/Databricks-115/IPText.json"
-
-  val IPv4DF: DataFrame = spark.read.json(path)
-
-  IPv4DF.createOrReplaceTempView("IPv4")
-
- */
-
-/*
-
-  val addrL: Long = IPv4ToLong(addr)
-  require(isIP(addr), "IPv4 invalid.")
-
-  //compare operations
-  def <(that: IPv4): Boolean = this.addrL < that.addrL
-  def >(that: IPv4): Boolean = this.addrL > that.addrL
-  def <=(that: IPv4): Boolean = this.addrL <= that.addrL
-  def >=(that: IPv4): Boolean = this.addrL >= that.addrL
-
-  //Return network address of IP address
-  def mask(maskIP: Int): IPv4 = {
-    require(maskIP >= 0 && maskIP <= 32, "Can only mask 0-32.")
-    longToIPv4(0xFFFFFFFF << (32 - maskIP) & addrL)
-  }
-  def mask(maskIP: String): IPv4 = {
-    require(isIP(maskIP), "IPv4 invalid.")
-    longToIPv4(IPv4ToLong(maskIP) & addrL)
-  }
-
-  // Address Types
-  val isMulticast: Boolean = if (addrL >= 3758096384L && addrL <= 4026531839L) true else false
-  val isPrivate: Boolean = if (
-    (addrL >= 167772160L && addrL <= 184549375L) ||
-      (addrL >= 2886729728L && addrL <= 2887778303L) ||
-      (addrL >= 3232235520L && addrL <= 3232301055L)
-  ) true else false
-  val isGlobal: Boolean = !isPrivate
-  val isUnspecified: Boolean = if (addrL == 0) true else false
-  val isLoopback: Boolean = if (addrL >= 2130706432L && addrL <= 2147483647L) true else false
-  val isLinkLocal: Boolean = if (addrL >= 2851995648L && addrL <= 2852061183L) true else false
-  val isReserved: Boolean = if (
-    (addrL >= 0L && addrL <= 16777215L) ||
-      isPrivate ||
-      (addrL >= 1681915904L && addrL <= 1686110207L) ||
-      isLoopback ||
-      isLinkLocal ||
-      (addrL >= 3221225472L && addrL <= 3221225727L) ||
-      (addrL >= 3221225984L && addrL <= 3221226239L) ||
-      (addrL >= 3227017984L && addrL <= 3227018239L) ||
-      (addrL >= 3323068416L && addrL <= 3323199487L) ||
-      (addrL >= 3325256704L && addrL <= 3325256959L) ||
-      (addrL >= 3405803776L && addrL <= 3405804031L) ||
-      isMulticast ||
-      (addrL >= 4026531840L && addrL <= 4294967294L) ||
-      (addrL == 4294967295L)
-  ) true else false
- */
