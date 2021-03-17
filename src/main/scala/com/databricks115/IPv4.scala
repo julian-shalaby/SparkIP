fee0ba86b6d06fbe6bb841d2ed37132cfc476aed
Julian Shalaby <jshalaby@ucsc.edu>
2021-03-16 17:49:28 -0700

    code trim

--- src/main/scala/com/databricks115/IPv4.scala
+++ src/main/scala/com/databricks115/IPv4.scala
@@ -30,26 +30,25 @@ case class IPv4(ipaddress: String) extends Ordered[IPv4] with IPv4Traits {
     // Address Types
     lazy val isMulticast: Boolean = addrL >= 3758096384L && addrL <= 4026531839L
     lazy val isPrivate: Boolean = (addrL >= 167772160L && addrL <= 184549375L) ||
-          (addrL >= 2886729728L && addrL <= 2887778303L) ||
-          (addrL >= 3232235520L && addrL <= 3232301055L)
+      (addrL >= 2886729728L && addrL <= 2887778303L) ||
+      (addrL >= 3232235520L && addrL <= 3232301055L)
     lazy val isGlobal: Boolean = !isPrivate
     lazy val isUnspecified: Boolean = addrL == 0
     lazy val isLoopback: Boolean = addrL >= 2130706432L && addrL <= 2147483647L
     lazy val isLinkLocal: Boolean = addrL >= 2851995648L && addrL <= 2852061183L
     lazy val isReserved: Boolean = (addrL >= 0L && addrL <= 16777215L) ||
-          isPrivate ||
-          (addrL >= 1681915904L && addrL <= 1686110207L) ||
-          isLoopback ||
-          isLinkLocal ||
-          (addrL >= 3221225472L && addrL <= 3221225727L) ||
-          (addrL >= 3221225984L && addrL <= 3221226239L) ||
-          (addrL >= 3227017984L && addrL <= 3227018239L) ||
-          (addrL >= 3323068416L && addrL <= 3323199487L) ||
-          (addrL >= 3325256704L && addrL <= 3325256959L) ||
-          (addrL >= 3405803776L && addrL <= 3405804031L) ||
-          isMulticast ||
-          (addrL >= 4026531840L && addrL <= 4294967294L) ||
-          (addrL == 4294967295L)
+      isPrivate ||
+      (addrL >= 1681915904L && addrL <= 1686110207L) ||
+      isLoopback || isLinkLocal ||
+      (addrL >= 3221225472L && addrL <= 3221225727L) ||
+      (addrL >= 3221225984L && addrL <= 3221226239L) ||
+      (addrL >= 3227017984L && addrL <= 3227018239L) ||
+      (addrL >= 3323068416L && addrL <= 3323199487L) ||
+      (addrL >= 3325256704L && addrL <= 3325256959L) ||
+      (addrL >= 3405803776L && addrL <= 3405804031L) ||
+      isMulticast ||
+      (addrL >= 4026531840L && addrL <= 4294967294L) ||
+      (addrL == 4294967295L)
 
     //interface with ipv6
     private def IPv4to2IPv6Octets(ip: IPv4): String = s"${(ip.addrL >> 16 & 0xFFFF).toHexString}:${(ip.addrL & 0xFFFF).toHexString}"
