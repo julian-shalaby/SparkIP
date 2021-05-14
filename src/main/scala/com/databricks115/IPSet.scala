bc1ccd98c3dd62681d67749e4769f9938b1d9cf4
Julian Shalaby <jshalaby@ucsc.edu>
2021-05-14 06:51:09 -0700

    new basic IPSet

--- src/main/scala/com/databricks115/IPSet.scala
+++ src/main/scala/com/databricks115/IPSet.scala
@@ -1,136 +1,100 @@
 package com.databricks115
 
-case class IPSet(ipAddresses: Any*) {
+case class IPSet(input: Any*) {
     def this() = this(null)
     var ipMap: Map[String, Either[Long, BigInt]] = Map()
-    var netAVL:AVLTree = AVLTree()
-    var root: Node = _
+//    var netAVL:AVLTree = AVLTree(
+//    var root: Node = _
 
     private def initializeSet(): Unit = {
-        ipAddresses.foreach {
+        input.foreach {
             case s: String =>
-                val v4 = try {
-                    Some(IPv4(s))
-                }
-                catch {
+                val ip = try {
+                    Some(IPAddress(s))
+                } catch {
                     case _: Throwable => None
                 }
-                val v6 = try {
-                    Some(IPv6(s))
+                //                val net = try {
+                //                    Some(IPNetwork(s))
+                //                } catch {
+                //                    case _: Throwable => None
+                //                }
+
+                if (ip.isDefined) {
+                    ip.get.addrNum match {
+                        case Left(value) => ipMap += (s -> Left(value))
+                        case Right(value) => ipMap += (s -> Right(value))
+                    }
                 }
-                catch {
-                    case _: Throwable => None
-                }
-                val v4Net = try {
-                    Some(IPv4Network(s))
-                }
-                catch {
-                    case _: Throwable => None
-                }
-                val v6Net = try {
-                    Some(IPv6Network(s))
-                }
-                catch {
-                    case _: Throwable => None
-                }
-                if (v4.isDefined) ipMap += (s -> Left(v4.get.addrL))
-                else if(v6.isDefined) ipMap += (s -> Right(v6.get.addrBI))
-                else if(v4Net.isDefined) root = netAVL.insert(root, v4Net.get)
-                else if(v6Net.isDefined) root = netAVL.insert(root, v6Net.get)
+                // else if(net.isDefined) root = netAVL.insert(root, net.get)
                 else throw new Exception("Bad input.")
-            case v4: IPv4 => ipMap += (v4.ipAddress -> Left(v4.addrL))
-            case v6: IPv6 => ipMap += (v6.ipAddress -> Right(v6.addrBI))
-            case v4Net: IPv4Network => root = netAVL.insert(root, v4Net)
-            case v6Net: IPv6Network => root = netAVL.insert(root, v6Net)
+            case ip: IPAddress =>
+                ip.addrNum match {
+                    case Left(value) => ipMap += (ip.addr -> Left(value))
+                    case Right(value) => ipMap += (ip.addr -> Right(value))
+                }
+            //case net: IPNetwork => root = netAVL.insert(root, net)
             case _ => throw new Exception("Bad input.")
         }
     }
     initializeSet()
 
-    def add(ip: Any*): Unit = {
-        ip.foreach {
+    def add(ips: Any*): Unit = {
+        ips.foreach {
             case s: String =>
-                val v4 = try {
-                    Some(IPv4(s))
-                }
-                catch {
+                val ip = try {
+                    Some(IPAddress(s))
+                } catch {
                     case _: Throwable => None
                 }
-                val v6 = try {
-                    Some(IPv6(s))
+//                val net = try {
+//                    Some(IPNetwork(s))
+//                } catch {
+//                    case _: Throwable => None
+//                }
+
+                if (ip.isDefined) {
+                    ip.get.addrNum match {
+                        case Left(value) => ipMap += (s -> Left(value))
+                        case Right(value) => ipMap += (s -> Right(value))
+                    }
                 }
-                catch {
-                    case _: Throwable => None
-                }
-                val v4Net = try {
-                    Some(IPv4Network(s))
-                }
-                catch {
-                    case _: Throwable => None
-                }
-                val v6Net = try {
-                    Some(IPv6Network(s))
-                }
-                catch {
-                    case _: Throwable => None
-                }
-                if (v4.isDefined) ipMap += (s -> Left(v4.get.addrL))
-                else if(v6.isDefined) ipMap += (s -> Right(v6.get.addrBI))
-                else if(v4Net.isDefined) root = netAVL.insert(root, v4Net.get)
-                else if(v6Net.isDefined) root = netAVL.insert(root, v6Net.get)
+                // else if(net.isDefined) root = netAVL.insert(root, net.get)
                 else throw new Exception("Bad input.")
-            case v4: IPv4 => ipMap += (v4.ipAddress -> Left(v4.addrL))
-            case v6: IPv6 => ipMap += (v6.ipAddress -> Right(v6.addrBI))
-            case v4Net: IPv4Network => root = netAVL.insert(root, v4Net)
-            case v6Net: IPv6Network => root = netAVL.insert(root, v6Net)
+            case ip: IPAddress =>
+                ip.addrNum match {
+                    case Left(value) => ipMap += (ip.addr -> Left(value))
+                    case Right(value) => ipMap += (ip.addr -> Right(value))
+                }
+            //case net: IPNetwork => root = netAVL.insert(root, net)
             case _ => throw new Exception("Bad input.")
         }
     }
-    def remove(ip: Any*): Unit = {
-        ip.foreach {
+    def remove(ips: Any*): Unit = {
+        ips.foreach {
             case s: String =>
-                val v4 = try {
-                    Some(IPv4(s))
-                }
-                catch {
+                val ip = try {
+                    Some(IPAddress(s))
+                } catch {
                     case _: Throwable => None
                 }
-                val v6 = try {
-                    Some(IPv6(s))
-                }
-                catch {
-                    case _: Throwable => None
-                }
-                val v4Net = try {
-                    Some(IPv4Network(s))
-                }
-                catch {
-                    case _: Throwable => None
-                }
-                val v6Net = try {
-                    Some(IPv6Network(s))
-                }
-                catch {
-                    case _: Throwable => None
-                }
-                if (v4.isDefined || v6.isDefined) ipMap -= s
-                else if (v4Net.isDefined) root = netAVL.delete(root, v4Net.get)
-                else if (v6Net.isDefined) root = netAVL.delete(root, v6Net.get)
+                //                val net = try {
+                //                    Some(IPNetwork(s))
+                //                } catch {
+                //                    case _: Throwable => None
+                //                }
+                if (ip.isDefined) ipMap -= s
+//                else if (net.isDefined) root = netAVL.delete(root, net.get)
                 else throw new Exception("Bad input.")
-            case v4: IPv4 => ipMap -= v4.ipAddress
-            case v6: IPv4 => ipMap -= v6.ipAddress
+            case ip: IPAddress => ipMap -= ip.addr
             case _ => throw new Exception("Bad input.")
         }
     }
-    def contains(ip: Any*): Boolean = {
-        ip.foreach {
-            case s: String => if (!ipMap.contains(s) && !netAVL.AVLSearch(root, s) && !netAVL.AVLSearchIP(root, s))
-                return false
-            case v4: IPv4 => if (!ipMap.contains(v4.ipAddress) && !netAVL.AVLSearchIP(root, v4))
-                return false
-            case v6: IPv6 => if (!ipMap.contains(v6.ipAddress) && !netAVL.AVLSearchIP(root, v6)) return false
-            case v4Net: IPv4Network => if (!netAVL.AVLSearch(root, v4Net)) return false
-            case v6Net: IPv4Network => if (!netAVL.AVLSearch(root, v6Net)) return false
+    def contains(ips: Any*): Boolean = {
+        ips.foreach {
+            case s: String => if (!ipMap.contains(s)) return false
+            case ip: IPAddress => if (!ipMap.contains(ip.addr)) return false
+//            case net: IPNetwork => if (!netAVL.AVLSearch(root, net)) return false
             case _ => throw new Exception("Bad input.")
         }
         true
--- src/test/scala/com/databricks115/TestIPSet.scala
+++ src/test/scala/com/databricks115/TestIPSet.scala
@@ -4,11 +4,10 @@ import org.scalatest.FunSuite
 class TestIPSet extends FunSuite {
 
   test("Constructor") {
-    val set1 = IPSet("192.0.0.0", "::", "225.0.0.0/8")
-    val ip1 = IPv4("192.0.0.0")
-    val ip2 = IPv6("::")
-    val net1 = IPv4Network("225.0.0.0/8")
-    val set2 = IPSet(ip1, ip2, net1)
+    val set1 = IPSet("192.0.0.0", "::")
+    val ip1 = IPAddress("192.0.0.0")
+    val ip2 = IPAddress("::")
+    val set2 = IPSet(ip1, ip2)
     val set3 = IPSet()
 
     assert(set1.ipMap == set2.ipMap)
@@ -17,30 +16,27 @@ class TestIPSet extends FunSuite {
 
   test("Add") {
     val set1 = IPSet()
-    set1.add("192.0.0.0", "::", "::/16")
-    val ip1 = IPv4("192.0.0.0")
-    val ip2 = IPv6("::")
-    val net1 = IPv6Network("::/16")
+    set1.add("192.0.0.0", "::")
+    val ip1 = IPAddress("192.0.0.0")
+    val ip2 = IPAddress("::")
     val set2 = IPSet()
-    set2.add(ip1, ip2, net1)
+    set2.add(ip1, ip2)
 
     assert(set1.ipMap == set2.ipMap)
   }
 
   test("Remove") {
-    val set1 = IPSet("192.0.0.0", "::", "225.0.0.0/8", "::/16")
+    val set1 = IPSet("192.0.0.0", "::")
     val set2 = IPSet()
-    set1.remove("192.0.0.0", "::", "225.0.0.0/8", "::/16")
+    set1.remove("192.0.0.0", "::")
 
     assert(set1.ipMap == set2.ipMap)
-    assert(set1.netAVL.returnAll(set1.root) == set2.netAVL.returnAll(set2.root))
   }
 
   test("Contains") {
-    val set1 = IPSet("192.0.0.0", "::", "225.0.0.0/8", "::/16")
+    val set1 = IPSet("192.0.0.0", "::")
 
-    assert(set1.contains("225.0.5.0"))
-    assert(set1.contains("225.0.0.0/8"))
+    assert(set1.contains("192.0.0.0"))
   }
 
 }
